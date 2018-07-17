2bda2c23c491a7a7ec0e75ca07888a4ec12e42f5
soso-su <975824138@qq.com>
2018-07-17 10:55:41 +0800

    添加商城首页部分UI

---
+++ YTproject/Class/Store/YTRecommendViewController.m
@@ -0,0 +1,37 @@
+//
+//  YTRecommendViewController.m
+//  YTproject
+//
+//  Created by suhuahao on 2018/7/17.
+//  Copyright © 2018年 suhuahao. All rights reserved.
+//
+
+#import "YTRecommendViewController.h"
+
+@interface YTRecommendViewController ()
+
+@end
+
+@implementation YTRecommendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
