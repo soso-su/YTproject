a367530a9dd797eac400227dcb43a42abfc051fa
soso-su <975824138@qq.com>
2018-07-27 01:00:10 +0800

    no message

---
+++ YTproject/Class/Mine/Attestation/YTAttextationFailViewController.m
@@ -0,0 +1,26 @@
+//
+//  YTAttextationFailViewController.m
+//  YTproject
+//
+//  Created by suhuahao on 2018/7/26.
+//  Copyright © 2018年 suhuahao. All rights reserved.
+//
+
+#import "YTAttextationFailViewController.h"
+
+@interface YTAttextationFailViewController ()
+
+@end
+
+@implementation YTAttextationFailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)reAttextation:(UIButton *)sender {
+}
+
+
+@end
